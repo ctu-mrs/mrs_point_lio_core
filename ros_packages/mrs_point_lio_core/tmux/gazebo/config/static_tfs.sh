df1a312bd739100d7441b9aa78189977028995d3
Tomas Baca <Tomas Baca>
2025-11-14 10:37:53 +0100

    udpated example tmuxes

--- ros_packages/mrs_point_lio_core/tmux/gazebo/config/static_tfs.sh
+++ ros_packages/mrs_point_lio_core/tmux/gazebo/config/static_tfs.sh
@@ -8,4 +8,4 @@ ros2 run tf2_ros static_transform_publisher \
   --pitch 0 \
   --yaw 0.0 \
   --frame-id $UAV_NAME/fcu \
-  --child-frame-id $UAV_NAME/lidar
+  --child-frame-id $UAV_NAME/ouster_link
